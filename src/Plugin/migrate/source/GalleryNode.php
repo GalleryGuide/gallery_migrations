0ef62a03cf41a5e3aa017fd844554661e26e1c59
Malcolm Young <malcolm.young@capgemini.com>
2016-04-28 11:19:11 +0100

    get location data from d6 database

--- src/Plugin/migrate/source/GalleryNode.php
+++ src/Plugin/migrate/source/GalleryNode.php
@@ -42,6 +42,122 @@ class GalleryNode extends D6Node {
       $row->setSourceProperty('field_website', $website);
     }
     
+    // Get the location data from the D6 database.
+    $nid = $row->getSourceProperty('nid');    
+    $location = $this->getLocation($nid);
+    
+    // Set up latitude and longitude for use with geolocation module.
+    $geolocation = $this->prepareGeoLocation($location->latitude, $location->longitude);
+    $row->setSourceProperty('field_location', $geolocation);
+    
+    $address = $this->prepareAddress($location);
+    $row->setSourceProperty('field_address', $address);
+
     return parent::prepareRow($row);
   }
+
+  /**
+   * Get the location for this node from the D6 database. 
+   * 
+   * @param int $nid
+   * 
+   * @return array
+   */
+  protected function getLocation($nid) {
+    // Get the location ID.
+    
+    // select * from drupal_location where lid = (select lid from drupal_location_instance where nid = 5732)\G
+    \Drupal\Core\Database\Database::setActiveConnection('d6');
+    $db = \Drupal\Core\Database\Database::getConnection();
+
+    $query = $db->select('location_instance', 'li');
+    $query->join('location', 'l', 'l.lid = li.lid');
+    $query->condition('nid', $nid)
+      ->fields('l', array(
+        'lid',
+        'name',
+        'street',
+        'additional',
+        'city',
+        'province',
+        'postal_code',
+        'country',
+        'latitude',
+        'longitude',
+        'source',
+      ));
+    
+    $result = $query->execute();
+    
+    \Drupal\Core\Database\Database::setActiveConnection();
+
+    $data = array();
+    
+    foreach ($result as $row) {
+      $data[] = $row;
+    }
+    return $data[0];
+  }
+
+  /**
+   * Prepare geolocation data for use in D8 geolocation field.
+   * 
+   * @param string $latitude
+   * @param string $longitude
+   * @return array
+   *   An array to pass to the geolocation field.
+   */
+  protected function prepareGeoLocation($latitude, $longitude) {
+    $location = array(
+      array(
+        'lat' => $latitude,
+        'lng' => $longitude,
+      ),
+    );
+    return $location;
+  }
+
+  /**
+   * Prepare data from a location object for an address field.
+   * 
+   * @param Object $location
+   *   The location object, selected from the D6 database.
+   *
+   * @return array
+   *   An array of values to use in an address field.
+   */
+  protected function prepareAddress($location) {
+    return array(
+      array(
+        'country_code' => $this->convertCountry($location->country),
+        'locality' => $location->city,
+        'postal_code' => $location->postal_code,
+        'address_line1' => $location->street,
+        'address_line2' => $location->additional,
+      ),
+    );
+  }
+
+  /**
+   * Convert country codes from D6 location format.
+   *
+   * Yes, it's hacky, but I know that the database only contains 3 values.
+   *
+   * @param string $country
+   *   The old country code.
+   * 
+   * @return string
+   *   The ISO country code.
+   */
+  protected function convertCountry($country) {
+    
+    $country_codes = array(
+      'uk' => 'GB',
+      'pt' => 'PT',
+      'nl' => 'NL',
+    );
+    
+    return $country_codes[$country];
+  }
+  
 }
