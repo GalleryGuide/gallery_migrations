af19f24ab0d34978a3f43ca145945d9d683531a0
Malcolm Young <malcolm.young@capgemini.com>
2016-04-28 22:29:08 +0100

    process taxonomy terms for artist nodes

--- src/Plugin/migrate/source/GalleryTerm.php
+++ src/Plugin/migrate/source/GalleryTerm.php
@@ -11,7 +11,7 @@ use Drupal\migrate\Row;
 use Drupal\taxonomy\Plugin\migrate\source\Term;
 
 /**
- * Custom Drupal 6 node source from database.
+ * Custom Drupal 6 term source from database.
  *
  * @MigrateSource(
  *   id = "upgrade_d6_taxonomy_term"
@@ -51,12 +51,38 @@ Surname	field_surname	Computed	Configure  Remove
     $term_nid_vid = $this->getTermNidVid($tid);
     $term_node = $this->getTermNode($term_nid_vid);
     
-    print_r($row);
-    print_r($term_node);
+    $vid = $row->getSourceProperty('vid');
+    $vocabulary = $this->getVocab($vid);
+    $row->setSourceProperty('vid', $vocabulary);
+    
+    if (!empty($term_node->field_born_value)) {
+      $row->setSourceProperty('field_born', $term_node->field_born_value);      
+    }
+
+    if (!empty($term_node->field_born_value2)) {
+      $row->setSourceProperty('field_died', $term_node->field_born_value2);
+    }
+
+    if (!empty($term_node->field_artist_website_url)) {
+      $row->setSourceProperty('field_website', $term_node->field_artist_website_url);
+    }
+
+    if (!empty($term_node->body)) {
+      $row->setSourceProperty('field_biography', $term_node->body);
+    }
 
     return parent::prepareRow($row);
   }
-  
+
+  /**
+   * Get the relevant node and revision IDs for a taxonomy term node.
+   * 
+   * @param int $tid
+   *   The term ID.
+   *
+   * @return Object
+   *   The database result row object.
+   */
   protected function getTermNidVid($tid) {
     \Drupal\Core\Database\Database::setActiveConnection('d6');
     $db = \Drupal\Core\Database\Database::getConnection();
@@ -78,7 +104,15 @@ Surname	field_surname	Computed	Configure  Remove
     }
     return $data[0];
   }
-  
+
+  /**
+   * Get the taxonomy term node data.
+   * 
+   * @param Object $nid_vid
+   *   The database result row object with node and revision IDs.
+   * @return Object
+   *   The database result row object for the taxonomy term node.
+   */
   protected function getTermNode($nid_vid) {
     $nid = $nid_vid->nid;
     $vid = $nid_vid->vid;
@@ -87,7 +121,7 @@ Surname	field_surname	Computed	Configure  Remove
 
     $query = $db->select('content_type_artist', 'cta');
     $query->join('node_revisions', 'nr', 'cta.nid = nr.nid AND cta.vid = nr.vid');
-    $query->condition('nid', $nid)
+    $query->condition('cta.nid', $nid)
       ->condition('cta.vid', $vid)
       ->fields('cta', array(
         'field_born_value',
@@ -108,5 +142,38 @@ Surname	field_surname	Computed	Configure  Remove
     }
     return $data[0];
 
+  }
+
+  /**
+   * Convert a D6 vocabulary ID to a D8 vocabulary name.
+   * 
+   * @param int $vid
+   *   The D6 vocabulary ID.
+   *
+   * @return string
+   *   The D8 vocabulary name.
+   */
+  protected function getVocab($vid) {
+    \Drupal\Core\Database\Database::setActiveConnection('d6');
+    $db = \Drupal\Core\Database\Database::getConnection();
+
+    $query = $db->select('vocabulary', 'v');
+    $query->fields('v', array(
+        'vid',
+        'name',
+      ));
+
+    $result = $query->execute();
+
+    \Drupal\Core\Database\Database::setActiveConnection();
+
+    $data = array();
+    foreach ($result as $row) {
+      $old_vid = $row->vid;
+      
+      $data[$old_vid] = strtolower($row->name);
+    }
+    
+    return $data[$vid];
   }
 }
