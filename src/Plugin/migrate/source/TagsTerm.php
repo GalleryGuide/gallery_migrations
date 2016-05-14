89b0f6a1521b5d5015cea74e0f3f2e6d372cfa3d
Malcolm Young <malcolm.young@capgemini.com>
2016-05-14 06:11:45 +0100

    get term node migration working for tags

---
+++ src/Plugin/migrate/source/TagsTerm.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Drupal\gallery_migrations\Plugin\migrate\source;
+
+use Drupal\migrate\Row;
+use Drupal\taxonomy\Plugin\migrate\source\d6\TermNode;
+
+/**
+ * Source returning tids from the term_node table for the current revision.
+ *
+ * @MigrateSource(
+ *   id = "term_node_tags",
+ *   source_provider = "taxonomy"
+ * )
+ */
+class TagsTerm extends TermNode {
+  /**
+   * {@inheritdoc}
+   */
+  public function prepareRow(Row $row) {
+    if (parent::prepareRow($row) === FALSE) {
+      return FALSE;
+    }
+
+    return parent::prepareRow($row);
+  }
+}
