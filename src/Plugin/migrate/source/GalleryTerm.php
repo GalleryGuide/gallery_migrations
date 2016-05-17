56ae9377a26754003a36316861ff281fc2ca9eec
Malcolm Young <malcolm.young@capgemini.com>
2016-05-17 10:36:42 +0100

    update term node aliases

--- src/Plugin/migrate/source/GalleryTerm.php
+++ src/Plugin/migrate/source/GalleryTerm.php
@@ -9,6 +9,7 @@ namespace Drupal\gallery_migrations\Plugin\migrate\source;
 
 use Drupal\migrate\Row;
 use Drupal\taxonomy\Plugin\migrate\source\Term;
+use Drupal\Core\Database\Database;
 
 /**
  * Custom Drupal 6 term source from database.
@@ -35,16 +36,23 @@ class GalleryTerm extends Term {
       return FALSE;
     }
 
+    // Set the vocabulary explicitly because we're not migrating them.
     $tid = $row->getSourceProperty('tid');
     $vid = $row->getSourceProperty('vid');
     $vocabulary = $this->getVocab($vid);
     $row->setSourceProperty('vid', $vocabulary);
 
     if ($vocabulary == 'artists') {
-      // Get the related term node.
+      // Get the related term node and import its fields.
       $term_nid = $this->getTermNid($tid);
       $term_vid = $this->getRevisionId($term_nid);
       $term_node = $this->getTermNode($term_nid, $term_vid);
+
+      // TODO: this still isn't working...
+      $images = $this->getArtistImages($term_nid);
+      if (!empty($images)) {
+        $row->setSourceProperty('field_images', $images);
+      }
 
       $born = $term_node->field_born_value;
       if (!empty($born)) {
@@ -69,6 +77,20 @@ class GalleryTerm extends Term {
           'format' => 'filtered_html',
         );
         $row->setSourceProperty('description', $body);
+      }
+
+      // Update the term node alias to point to the term.
+      $alias = $this->getArtistUrl($term_nid);
+      if (!empty($alias)) {
+
+        $node_alias = '/node/' . $term_nid;
+        $term_alias = '/taxonomy/term' . $tid;
+
+        $connection = Database::getConnection();
+        $connection->update('url_alias')
+          ->fields(array('source' => $term_alias))
+          ->condition('source', $node_alias)
+          ->execute();
       }
     }
 
@@ -204,5 +226,69 @@ class GalleryTerm extends Term {
     }
     
     return $data[$vid];
+  }
+
+  /**
+   * Get the alias for a taxonomy term node.
+   *
+   * @param int $nid
+   *   The node ID.
+   * @return string
+   *   The alias.
+   */
+  protected function getArtistUrl($nid) {
+    \Drupal\Core\Database\Database::setActiveConnection('d6');
+    $db = \Drupal\Core\Database\Database::getConnection();
+
+    $source_path = 'node/' . $nid;
+    $query = $db->select('url_alias', 'ua');
+    $query->condition('src', $source_path);
+    $query->fields('ua', array(
+      'dst',
+    ));
+
+    $result = $query->execute();
+
+    \Drupal\Core\Database\Database::setActiveConnection();
+
+    foreach ($result as $row) {
+      $alias = $row->dst;
+    }
+    return $alias;
+  }
+
+  /**
+   * Get the associated images for an artist term node.
+   *
+   * @param int $nid
+   *   The node ID.
+   * @return array
+   *   The file details.
+   */
+  protected function getArtistImages($nid) {
+    \Drupal\Core\Database\Database::setActiveConnection('d6');
+    $db = \Drupal\Core\Database\Database::getConnection();
+
+    $query = $db->select('content_field_images', 'cfi');
+    $query->condition('nid', $nid);
+    $query->fields('cfi', array(
+      'field_images_fid',
+      'field_images_data',
+    ));
+
+    $result = $query->execute();
+
+    \Drupal\Core\Database\Database::setActiveConnection();
+
+    $data = array();
+    foreach ($result as $key => $row) {
+      $data[$key] = array(
+        'delta' => $key,
+        'fid' => $row->field_images_fid,
+        'list' => 1,
+        'data' => $row->field_images_data,
+      );
+    }
+    return $data;
   }
 }
